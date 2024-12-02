d8de4f38e5b7e7af759a2615fc9fb96ff76578a7
SoraHrS <soherreras@unal.edu.co>
2024-12-02 17:09:59 -0500

    se realizó el ejercicio 24

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        double pesoa, pesob, pesoc;
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+        System.out.println("Ingrese el peso de la esfera A: ");
+        pesoa = sc.nextDouble();
+        System.out.println("Ingrese el peso de la esfera B: ");
+        pesob = sc.nextDouble();
+        System.out.println("Ingrese el peso de la esfera C: ");
+        pesoc = sc.nextDouble();
 
-        System.out.println("Hello world!");
+        if (pesoa>pesob && pesoa>pesoc){
+            System.out.println("La esfera de mayor peso es A");
+        }else if (pesob>pesoa && pesob>pesoc){
+            System.out.println("La esfera de mayor peso es B");
+        }else{
+            System.out.println("La esfera de mayor peso es C");
+        }
     }
 }
